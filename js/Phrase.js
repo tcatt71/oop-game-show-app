7d7c11c729641c0d83ed58be0e34c3459dd0435b
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2023-04-06 01:43:52 -0700

    Add addPhraseToDisplay method to the Phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,4 +6,19 @@ class Phrase {
   constructor(phrase) {
     this.phrase = phrase.toLowerCase();
   }
+
+  addPhraseToDisplay() {
+    const phraseUl = document.querySelector('#phrase ul');
+
+    for (const char of this.phrase) {
+      const letterPlaceholderLi = document.createElement('LI');
+      if (char === ' ') {
+        letterPlaceholderLi.classList.add('space');
+      } else {
+        letterPlaceholderLi.classList.add('hide', 'letter', `${char}`);
+      }
+      phraseUl.insertAdjacentElement('beforeend', letterPlaceholderLi);
+    }
+  }
 }
+const newPhrase = new Phrase(myPhrase);
