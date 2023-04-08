0b51dd69bbfab763dee2d0b4b112998b0f9d0281
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2023-04-08 13:15:10 -0700

    Format phrase objects in the Phrase class

--- js/Game.js
+++ js/Game.js
@@ -6,11 +6,11 @@ class Game {
   constructor() {
     this.missed = 0;
     this.phrases = [
-      { phrase: 'This is a new year'},
-      { phrase: 'a bird in the hand is worth two in the bush'},
-      { phrase: 'hot on the heels'},
+      { phrase: 'This is a new year' },
+      { phrase: 'a bird in the hand is worth two in the bush' },
+      { phrase: 'hot on the heels' },
       { phrase: 'the darkest hour is just before the dawn' },
-      { phrase: 'the best defence is a good offence'}
+      { phrase: 'the best defence is a good offence' }
     ];
     this.activePhrase = null;
   }
