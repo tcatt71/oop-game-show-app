ba1d35a93d837f0dda34f5426941418ef2cf3bea
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2023-04-09 03:21:45 -0700

    Remove previous game's phrase when starting a new game

--- js/app.js
+++ js/app.js
@@ -3,9 +3,14 @@
  * app.js */
 
 const newGame = new Game();
-
 const startButton = document.querySelector('#btn__reset');
-startButton.addEventListener('click', () => newGame.startGame());
 
-const keyBoard = document.querySelector('#qwerty');
-keyBoard.addEventListener('click', (event) => newGame.handleInteraction(event));
+startButton.addEventListener('click', () => {
+  const phraseUL = document.querySelector('#phrase ul');
+  const phrase = phraseUL.querySelectorAll('li');
+
+  phrase.forEach(letter => phraseUL.removeChild(letter));
+
+
+const keyboard = document.querySelector('#qwerty');
+keyboard.addEventListener('click', (event) => newGame.handleInteraction(event));
