fc94c8f81588fb25a0d9ac812c8a44ef179cd7a5
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2023-04-08 20:37:10 -0700

    Add event listeners to the on screen keyboard

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
+ * app.js */
 
 const newGame = new Game();
 
 const startButton = document.querySelector('#btn__reset');
 startButton.addEventListener('click', () => newGame.startGame());
 
+const keyBoard = document.querySelector('#qwerty');
+keyBoard.addEventListener('click', (event) => newGame.handleInteraction(event));
